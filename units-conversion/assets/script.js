6f16337f5e708c50bf6cb41d0d07839964eaf462
Joel Warwick <60834182+jwarwick03@users.noreply.github.com>
2024-02-22 17:54:00 +0000

    finished unit conversion ajax

--- units-conversion/assets/script.js
+++ units-conversion/assets/script.js
@@ -8,48 +8,70 @@ function calculate() {
     if (form.valid()) {
         
         // Operand 1
-        var fromvalue = document.getElementById("fromvalue").value;
+        var Oprand1 = document.getElementById("Oprand1").value;
 
-        // Operator
+        // From Value Radio Buttons
         // Get the value associated with the operator that was checked (+, -, *, or /)
-        var operator;
-        if (document.getElementById("fromcentimeters").checked) {
-            operator = document.getElementById("fromcentimeters").value;
+        var FromUnit;
+        if (document.getElementById("fromcm").checked) {
+            FromUnit = document.getElementById("fromcm").value;
         }
-        if (document.getElementById("frommeters").checked) {
-            operator = document.getElementById("frommeters").value;
+        if (document.getElementById("fromm").checked) {
+            FromUnit = document.getElementById("fromm").value;
         }
-        if (document.getElementById("fromkilometers").checked) {
-            operator = document.getElementById("fromkilometers").value;
+        if (document.getElementById("fromkm").checked) {
+            FromUnit = document.getElementById("fromkm").value;
         }
-        if (document.getElementById("frominches").checked) {
-            operator = document.getElementById("frominches").value;
+        if (document.getElementById("fromin").checked) {
+            FromUnit = document.getElementById("fromin").value;
         }
-        if (document.getElementById("fromfeet").checked) {
-            operator = document.getElementById("fromfeet").value;
+        if (document.getElementById("fromft").checked) {
+            FromUnit = document.getElementById("fromft").value;
         }
-        if (document.getElementById("fromyards").checked) {
-            operator = document.getElementById("fromyards").value;
+        if (document.getElementById("fromyd").checked) {
+            FromUnit = document.getElementById("fromyd").value;
         }
-        if (document.getElementById("frommiles").checked) {
-            operator = document.getElementById("frommiles").value;
+        if (document.getElementById("frommi").checked) {
+            FromUnit = document.getElementById("frommi").value;
+        }
+
+        // To Value Radio Buttons
+        var ToUnit;
+        if (document.getElementById("tocm").checked) {
+            ToUnit = document.getElementById("tocm").value;
+        }
+        if (document.getElementById("tom").checked) {
+            ToUnit = document.getElementById("tom").value;
+        }
+        if (document.getElementById("tokm").checked) {
+            ToUnit = document.getElementById("tokm").value;
+        }
+        if (document.getElementById("toin").checked) {
+            ToUnit = document.getElementById("toin").value;
+        }
+        if (document.getElementById("toft").checked) {
+            ToUnit = document.getElementById("toft").value;
+        }
+        if (document.getElementById("toyd").checked) {
+            ToUnit = document.getElementById("toyd").value;
+        }
+        if (document.getElementById("tomi").checked) {
+            ToUnit = document.getElementById("tomi").value;
         }
 
         
-        // Operand 2
-        var operand2 = document.getElementById("Operand2").value;
 
-        CalculateResult(operand1, operator, operand2);
+        CalculateResult(Oprand1, FromUnit, ToUnit);
     }
 }
 
-async function CalculateResult(operand1, operator, operand2) {
+async function CalculateResult(Oprand1, FromUnit, ToUnit) {
         
         // URL and method used with AJAX Call
-        var myURL = "http://brucebauer.info/assets/ITEC3650/gethint.php";
+        var myURL = "https://brucebauer.info/assets/ITEC3650/unitsconversion.php";
 
         /* AJAX calculator requires Operand1, Operator, and Operand2 */
-        myURL = myURL + "?Operand1=" + encodeURIComponent(operand1) + "&Operator=" + encodeURIComponent(operator) + "&Operand2=" + encodeURIComponent(operand2);
+        myURL = myURL + "?FromValue=" + encodeURIComponent(Oprand1) + "&FromUnit=" + encodeURIComponent(FromUnit) + "&ToUnit=" + encodeURIComponent(ToUnit);
 
         /* fetch the results */
         let myCalcObject = await fetch(myURL);
@@ -62,15 +84,28 @@ function clearform() {
     "use strict";
     
     /* Set all of the form values to blank or false */
-    document.getElementById("Operand1").value = "";
+    document.getElementById("Oprand1").value = "";
     document.getElementById("Operand1Msg").innerHTML = "";
-    document.getElementById("AddOperator").checked = false;
-    document.getElementById("SubtractOperator").checked = false;
-    document.getElementById("MultiplyOperator").checked = false;
-    document.getElementById("DivideOperator").checked = false;
-    document.getElementById("OperatorMsg").innerHTML = "";
-    document.getElementById("Operand2").value = "";
-    document.getElementById("Operand2Msg").innerHTML = "";
+
+    document.getElementById("fromcm").checked = false;
+    document.getElementById("fromm").checked = false;
+    document.getElementById("fromkm").checked = false;
+    document.getElementById("fromin").checked = false;
+    document.getElementById("fromft").checked = false;
+    document.getElementById("fromyd").checked = false;
+    document.getElementById("frommi").checked = false;
+
+    document.getElementById("tocm").checked = false;
+    document.getElementById("tom").checked = false;
+    document.getElementById("tokm").checked = false;
+    document.getElementById("toin").checked = false;
+    document.getElementById("toft").checked = false;
+    document.getElementById("toyd").checked = false;
+    document.getElementById("tomi").checked = false;
+
+    document.getElementById("fromMsg").innerHTML = "";
+    document.getElementById("toMsg").innerHTML = "";
+
     document.getElementById("Result").innerHTML = "";
 }
 
